ba0e62330a82de81f7c49d6d95493d365ae6f9d6
Erik Harlan <harlanes14@gmail.com>
2019-06-15 16:54:32 -0600

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./TestComponent.scss";
+class TestComponent extends React.Component {
+  render() {
+    return <h1>Hello from Component!</h1>;
+  }
+}
+
+export default TestComponent;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: path.join(__dirname, "demo/src/index.html"),
+  filename: "./index.html"
+});
+
+module.exports = {
+  entry: path.join(__dirname, "src/index.js"),
+  output: {
+    path: path.join(__dirname, "build/dist"),
+    filename: "index.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin],
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  devServer: {
+    port: 3001
+  }
+};
